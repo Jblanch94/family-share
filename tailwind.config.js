4a6f52c8418ec130d31934b6530e72c4b62acfc9
Johnny Blanchard <johnnyblanchard94@gmail.com>
2022-04-09 10:29:55 -0400

    add custom colors for sidebar and hover colors on nav elements

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: "#2b2b2b",
+        darkHover: "#363636",
+        gray: "#cccccc",
+      },
+    },
   },
   plugins: [],
 };
