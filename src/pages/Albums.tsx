6a7845e46a7ef97d77fc0b071349871549493e03
Johnny Blanchard <johnnyblanchard94@gmail.com>
2022-05-07 10:59:33 -0400

    add form for searching for albums with the ui for a search bar

--- src/pages/Albums.tsx
+++ src/pages/Albums.tsx
@@ -1,12 +1,36 @@
+import { FormProvider, useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 
 import Fab from "../components/core/Fab";
 import Header from "../components/features/Header";
+import SearchAlbumsForm from "../components/features/SearchAlbumsForm";
+
+interface SearchAlbumsFormValues {
+  name: string;
+}
 
 const Albums = (): JSX.Element => {
+  const methods = useForm<SearchAlbumsFormValues>({
+    defaultValues: { name: "" },
+  });
+
+  const onSubmit = (formData: SearchAlbumsFormValues) => {
+    console.log(formData);
+    methods.reset();
+  };
+
   return (
     <>
       <Header title='Albums' />
+      <FormProvider {...methods}>
+        <form onSubmit={methods.handleSubmit(onSubmit)}>
+          <div className='mx-4 my-2'>
+            <SearchAlbumsForm />
+          </div>
+        </form>
+      </FormProvider>
+
+      {/* List to display all of the albums with Album Name, User who created the album and the number of photos in each album as cards in a grid */}
       <Link to='/albums/add'>
         <Fab />
       </Link>
