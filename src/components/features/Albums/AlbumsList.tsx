e6a8c6488e3b15bfe0841a78371ef0fb839a80ab
Johnny Blanchard <johnnyblanchard94@gmail.com>
2022-05-23 11:02:55 -0400

    add loading state for albums while data is being fetched

--- src/components/features/Albums/AlbumsList.tsx
+++ src/components/features/Albums/AlbumsList.tsx
@@ -1,7 +1,7 @@
-import { useEffect, useCallback, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useEffect, useReducer } from "react";
 
 import AlbumsListItem from "./AlbumsListItem";
+import LoadingIcon from "../../icons/LoadingIcon";
 import { useSupabase } from "../../../contexts/SupabaseContext";
 import { useAuth } from "../../../contexts/AuthContext";
 import { Album } from "../../../types/resources";
@@ -10,55 +10,102 @@ interface Profile {
   family_id: string;
 }
 
+enum ActionTypes {
+  ERROR = "ERROR",
+  FETCH_PROFILE = "FETCH_PROFILE",
+  FETCH_ALBUMS = "FETCH_ALBUMS",
+  SET_LOADING = "SET_LOADING",
+}
+
+type State = {
+  loading: boolean;
+  error?: string;
+  albums: Album[];
+  profile: Profile | null;
+};
+
+type Action =
+  | { type: ActionTypes.FETCH_PROFILE; payload: Profile }
+  | { type: ActionTypes.FETCH_ALBUMS; payload: Album[] }
+  | { type: ActionTypes.ERROR; payload: string }
+  | { type: ActionTypes.SET_LOADING; payload: boolean };
+
+function reducer(state: State, action: Action): State {
+  switch (action.type) {
+    case ActionTypes.SET_LOADING:
+      return { ...state, loading: action.payload };
+    case ActionTypes.FETCH_PROFILE:
+      return { ...state, profile: action.payload };
+    case ActionTypes.FETCH_ALBUMS:
+      return { ...state, albums: action.payload };
+    case ActionTypes.ERROR:
+      return { ...state, albums: [], profile: null, error: action.payload };
+    default:
+      return state;
+  }
+}
+
 const AlbumsList = () => {
-  const [albums, setAlbums] = useState<Album[] | null>([]);
-  const [profile, setProfile] = useState<Profile | null>(null);
-  const navigate = useNavigate();
+  const initialState = {
+    loading: false,
+    error: undefined,
+    profile: null,
+    albums: [],
+  };
+  const [state, dispatch] = useReducer(reducer, initialState);
   const { supabase } = useSupabase();
   const { user } = useAuth();
 
-  const fetchProfile = useCallback(async () => {
-    if (!user || !user.id) {
-      return navigate("/auth/login", { replace: true });
-    }
-    const { data, error } = await supabase
-      .from("profiles")
-      .select(`id, name, family_id`)
-      .eq("id", user.id);
-    if (error) throw error;
-    setProfile(data[0]);
-  }, [supabase, user, navigate]);
-
-  const fetchAlbums = useCallback(async () => {
-    if (!profile || !profile.family_id)
-      return navigate("/auth/login", { replace: true });
-    const { data, error } = await supabase
-      .from<Album>("albums")
-      .select(`id, name, created_at`)
-      .eq("family_id", profile.family_id);
-    if (error) throw error;
-    setAlbums(data);
-  }, [supabase, profile, navigate]);
-
+  // fetch the profile of the currently logged in user
   useEffect(() => {
-    fetchProfile();
-    if (profile?.family_id) {
-      fetchAlbums();
-    }
-  }, [fetchAlbums, fetchProfile, user, profile]);
+    const fetchProfile = async () => {
+      dispatch({ type: ActionTypes.SET_LOADING, payload: true });
+      if (user === null) throw new Error("User is not logged in!");
+      const { data, error } = await supabase
+        .from("profiles")
+        .select(`id, name, family_id`)
+        .eq("id", user.id);
+      if (error) throw error;
+      dispatch({ type: ActionTypes.FETCH_PROFILE, payload: data[0] });
+    };
 
-  if (albums && !albums.length) {
-    return <div>No Albums</div>;
-  }
+    fetchProfile();
+  }, [supabase, user]);
+
+  // fetch all albums associated with the family of the logged in user
+  useEffect(() => {
+    const fetchAlbums = async () => {
+      const { data, error } = await supabase
+        .from<Album>("albums")
+        .select(`id, name, created_at`)
+        .eq(
+          "family_id",
+          state.profile && state.profile.family_id
+            ? state.profile.family_id
+            : ""
+        );
+      if (error) throw error;
+      dispatch({ type: ActionTypes.FETCH_ALBUMS, payload: data });
+      dispatch({ type: ActionTypes.SET_LOADING, payload: false });
+    };
+
+    fetchAlbums();
+  }, [supabase, state.profile]);
 
   return (
     <>
+      {state.loading && (
+        <div className='flex justify-center items-center mt-12'>
+          <LoadingIcon size='large' />
+        </div>
+      )}
       <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-2 gap-x-2 mx-2'>
-        {albums?.map((album) => {
-          return (
-            <AlbumsListItem key={album.id} name={album.name} numPhotos={5} />
-          );
-        })}
+        {!state.loading &&
+          state.albums?.map((album) => {
+            return (
+              <AlbumsListItem key={album.id} name={album.name} numPhotos={5} />
+            );
+          })}
       </div>
     </>
   );
